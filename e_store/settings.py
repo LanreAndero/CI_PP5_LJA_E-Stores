f3df0e9a677fd66f0753bd7b41dff0c8c1f5c950
Lanre Andero <lanreandero@gmail.com>
2024-04-04 11:02:02 +0000

    added homepage content

--- e_store/settings.py
+++ e_store/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'store',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -81,7 +82,10 @@ ROOT_URLCONF = 'e_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
